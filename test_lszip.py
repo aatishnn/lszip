eb40475de655d5675d12c16eea16407053500555
Aatish Neupane <aatishnn@gmail.com>
2015-10-20 07:37:28 +0545

    Added tests for ECD related helper functions

--- test_lszip.py
+++ test_lszip.py
@@ -15,5 +15,24 @@ class HelpersTest(unittest.TestCase):
         header_range = lszip.generate_range_header(-20)
         self.assertEqual(header_range, {'Range': 'bytes=-20'})
 
+class ZipTest(unittest.TestCase):
+    def setUp(self):
+        # test_file.zip contains 1 text file with no archive comment
+        # test_file_comment.zip contains 1 text file with archive comment
+        # Archive comment
+        self.archive_comment = 'Hello THere\r\nI am fine.'
+        self.bytes = open('test_file.zip').read()
+        self.bytes_with_comment = open('test_file_comment.zip', 'rb').read()
+    def test_zip_get_ecd(self):
+        ecd = lszip.zip_get_ecd(self.bytes_with_comment)
+        self.assertIsNotNone(ecd)
+        
+        ecd = lszip.zip_get_ecd(self.bytes)
+        self.assertIsNotNone(ecd)
+
+        # Test ecd's no of files
+        self.assertEqual(ecd[lszip._ECD_ENTRIES_TOTAL], 1)
+
+
 if __name__ == '__main__':
     unittest.main()
